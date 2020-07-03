bdeded84e83891bfed03dc6918691640fe3c1f53
Tim Downey <tcdowney@users.noreply.github.com>
2020-07-03 11:07:26 -0700

    add additional raspi clock measurement

--- raspberry-pi/raspi-metrics.sh
+++ raspberry-pi/raspi-metrics.sh
@@ -12,11 +12,15 @@ current_cpu_temp_in_c() {
   /opt/vc/bin/vcgencmd measure_temp | cut -d"=" -f2 | cut -d"'" -f1
 }
 
-current_cpu_clock_speed() {
+actual_cpu_clock_speed() {
+  /opt/vc/bin/vcgencmd measure_clock arm | cut -d"=" -f2 | cut -d"'" -f1
+}
+
+desired_cpu_clock_speed() {
   cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_cur_freq
 }
 
 for i in `seq 1 $SECONDS`; do
- echo "$(iso8601_timestamp), $(current_cpu_temp_in_c), $(current_cpu_clock_speed)"
- sleep 1
+  echo "$(iso8601_timestamp), $(current_cpu_temp_in_c), $(actual_cpu_clock_speed), $(desired_cpu_clock_speed)"
+  sleep 1
 done
